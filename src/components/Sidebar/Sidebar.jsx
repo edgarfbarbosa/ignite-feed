ffc26ec229a4a85ba10a2f022207a3dfddcf3443
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-05-02 07:25:49 -0300

    finalizado o sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,27 @@
+import { PencilLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
     return (
-        <aside className={styles.sidebar}></aside>
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1587170194491-ce8e824631d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/96757357?v=4" />
+                <strong>Edgar Faria Barbosa</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
     )
 }
