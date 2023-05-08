d31d8a49459d5549582e96362ba7a2ea2fc0d17d
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-05-08 07:06:24 -0300

    finalizado header do componente post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,19 +1,19 @@
 import styles from './Post.module.css'
 
 export function Post() {
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/96757357?v=4" />
-                    <div className={styles.authorInfo}>
-                        <strong>Edgar Faria Barbosa</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/96757357?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Edgar Faria Barbosa</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
 
-                <time dateTime="">Publicado há: </time>
-            </header>
-        </article>
-    )
+        <time title="08 de Maio às 07:03" dateTime="2023-05-08 07:03:00">Publicado há 1h</time>
+      </header>
+    </article>
+  )
 }
