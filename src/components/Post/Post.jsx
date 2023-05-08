7c2515538c247506b23024e624e88be3c1c39cbd
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-05-08 07:15:09 -0300

    add: estrutura html de conteúdo do componente post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -13,7 +13,21 @@ export function Post() {
         </div>
 
         <time title="08 de Maio às 07:03" dateTime="2023-05-08 07:03:00">Publicado há 1h</time>
-      </header>
-    </article>
+
+        <div className={styles.content}>
+          <p>Fala galeraa 👋</p>
+
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+          <p>👉 <a href="">jane.design/doctorcare</a></p>
+
+          <p>
+            <a href="">#novoprojeto</a>
+            <a href="">#nlw</a>
+            <a href="">#rocketseat</a>
+          </p>
+        </div>
+      </header >
+    </article >
   )
 }
