bfe415970f207208efc935aa7b0293449f5b97bc
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-05-09 16:44:43 -0300

    add: criado o componente Comment

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/96757357?v=4" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Edgar Faria Barbosa</strong>
+                            <time title="08 de Maio às 07:03" dateTime="2023-05-08 07:03:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
