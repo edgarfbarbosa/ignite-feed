c489c03d0f57fa75e03c419d52085f6daeb10b22
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-05-11 17:46:48 -0300

    add: criado o componente Avatar e manipulado o mesmo dentro de outros elementos

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
+import { Avatar } from '../Avatar/Avatar'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://avatars.githubusercontent.com/u/96757357?v=4" alt="" />
+            <Avatar hasBorder={false} src="https://github.com/edgarfbarbosa.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { PencilLine } from '@phosphor-icons/react'
 
+import { Avatar } from '../Avatar/Avatar'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -11,7 +13,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/96757357?v=4" />
+                <Avatar src="https://github.com/edgarfbarbosa.png" />
                 <strong>Edgar Faria Barbosa</strong>
                 <span>Web Developer</span>
             </div>
