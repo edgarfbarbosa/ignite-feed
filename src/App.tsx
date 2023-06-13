25e9eb67cb107b9d9f5b9b9d71c801b6ad9a681f
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-06-13 11:54:45 -0300

    Criado nova branch para transferência dos arquivos em TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { Author, Content, Post } from './components/Post/Post'
+import { Header } from './components/Header/Header'
+import { Sidebar } from './components/Sidebar/Sidebar'
+
+import styles from './App.module.css'
+import './global.css'
+
+// Tonar a página dinâmica!
+
+// Componente Post:
+// * author: { avatarUrl: "", name: "", role: ""};
+// * content: String;
+// * publishedAt: Date;
+
+// Criando um array de posts:
+
+interface Post {
+  id: number;
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
+}
+
+const posts: Post[] = [
+  {
+    // Cada post deve ter um id único:
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/edgarfbarbosa.png',
+      name: 'Edgar Faria Barbosa',
+      role: 'Front-end Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-15 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-15 21:00:00')
+  },
+]
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </>
+  )
+}
